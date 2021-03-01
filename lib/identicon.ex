2e035042bc98af59a38ac2efb2b9d6d2f4dfe0c7
TaniaTenorio <taniatenorio@hotmail.com>
2021-03-01 16:36:25 -0600

    add documentation for has_input

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,9 +1,16 @@
 defmodule Identicon do
+  @moduledoc """
+  Provides method for creating an identicon based on a string
+  """
+
  def main(input) do
   input
   |> has_input
  end
 
+ @doc """
+  Hashes the provided input using the md5 algorithm and returns a list of 16 numbers
+ """
  def has_input(input) do
   :crypto.hash(:md5, input)
   |> :binary.bin_to_list
